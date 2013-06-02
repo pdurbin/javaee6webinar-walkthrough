711758c3eebe6caf71696a9d040264ef348664e7
Philip Durbin <philipdurbin@gmail.com>
2013-06-02 19:36:58 -0400

    added FriendEJB session bean

---
+++ src/java/org/glassfish/samples/FriendEJB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.glassfish.samples;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author pdurbin
+ */
+@Stateless
+public class FriendEJB {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
